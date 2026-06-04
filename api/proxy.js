ae923c2801e1df11ea4a96dddda2c51cd0808862
rishifaujdar535-png <rishifaujdar535@gmail.com>
2026-06-04 13:41:11 +0530

    Delete api/proxy.js

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  const base = "https://www.googleapis.com/youtube/v3";
-  const segment = req.url.includes("search") ? "/search" : "/videos";
-  const params = new URLSearchParams(req.query);
-  const response = await fetch(`${base}${segment}?${params}`);
-  const data = await response.json();
-  res.json(data);
-}
